5a43fa38e40f099fc4b682f3d5255a013423befe
Robert Nguyen <robert@gotitapp.co>
2019-05-09 12:00:52 +0700

    Fixing importing db problem

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'robert'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
