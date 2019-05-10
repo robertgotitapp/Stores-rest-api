3ccb06a4a2d3bedf47064310c140081b970aaf10
Robert Nguyen <robert@gotitapp.co>
2019-05-10 16:32:55 +0700

    Fixing code

--- resources/item.py
+++ resources/item.py
@@ -28,10 +28,10 @@ class Item(Resource):
 
 		data = Item.parser.parse_args()	
 		item = ItemModel( name, **data)
-		try:
-			item.save_to_db()
-		except: 
-			return {'message': 'An error occurred when inserting item {}.'.format(item.json())}, 500
+		# try:
+		item.save_to_db()
+		# except: 
+		# 	return {'message': 'An error occurred when inserting item {}.'.format(item.json())}, 500
 
 		return item.json(), 201
 
