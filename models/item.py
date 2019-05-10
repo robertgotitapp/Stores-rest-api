e8bfb3fac688df8482491a2c7cfbf2f112807331
Robert Nguyen <robert@gotitapp.co>
2019-05-10 16:30:03 +0700

    Updating item model

--- models/item.py
+++ models/item.py
@@ -16,7 +16,7 @@ class ItemModel(db.Model):
 		self.store_id = store_id
 
 	def json(self):
-		return {'name': self.name, 'price': self.price }
+		return {'name': self.name, 'price': self.price, 'store_id': self.store_id }
 
 	@classmethod
 	def find_by_name(cls, name):	
